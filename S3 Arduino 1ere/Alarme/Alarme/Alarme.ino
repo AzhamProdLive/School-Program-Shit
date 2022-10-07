d90e3ed7b28001b5dc33a911d5d80641e19a4d87
Azham Makes Trash <73955243+AzhamProdLive@users.noreply.github.com>
2022-10-07 21:36:20 +0200

    Add files via upload

---
+++ S3 Arduino 1ere/Alarme/Alarme/Alarme.ino
@@ -0,0 +1,32 @@
+const int Pin_contact=2;
+const int Pin_feux=3;
+const int Pin_porte=4;
+const int Pin_alarme=5;
+
+bool Contact;
+bool Feux;
+bool Porte;
+bool Alarme;
+
+
+void setup() {
+  pinMode(Pin_contact, INPUT);
+  pinMode(Pin_feux,INPUT);
+  pinMode(Pin_porte,INPUT);
+  pinMode(Pin_alarme,OUTPUT);
+}
+
+void loop() {
+  Porte=digitalRead(Pin_porte);
+  Feux=digitalRead(Pin_feux);
+  Contact=digitalRead(Pin_contact);
+  
+  if (Porte==1&&Feux==1||Contact==0&&Feux==1)
+  {
+    Alarme=1;
+  }else
+  {
+    Alarme=0;
+  }
+digitalWrite(Pin_alarme, Alarme);
+}
